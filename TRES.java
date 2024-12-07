7e1bb116898635c6f4b3a4166abc79aba8d1f744
JesvsRRR <jesusronaldrosales05@hotmail.com>
2024-12-07 09:07:50 -0500

    Add files via upload

---
+++ TRES.java
@@ -0,0 +1,48 @@
+
+package tres;
+
+import java.util.Scanner;
+
+public class TRES {
+    
+    public static int Sumatoria(int d){
+    Scanner sc = new Scanner(System.in);      
+    int[][]Matriz = new int[d][d];
+    int salida = 0;
+    
+    for(int i=0; i < d; i++){
+        for(int j=0; j < d;j++){
+            System.out.print("["+i+"]"+"[" + j+"]");
+            Matriz[i][j] = sc.nextInt();           
+        }
+    }    
+    
+    for(int i=0; i < d; i++){
+        if(i == 0 || i == d-1){
+        for(int j=0; j < d; j++){
+        salida += Matriz[i][j];    
+        }
+        }
+        else{
+        salida += Matriz[i][0];
+        salida += Matriz[i][d-1];
+        }
+    }
+    
+    return salida;
+    }
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresar dimension [d]: ");
+        int d = sc.nextInt();
+        
+        if(d <= 0){
+           d = sc.nextInt();;
+        }
+
+        int Suma_perimetro = Sumatoria(d);
+        System.out.println("SUMATORIA TOTAL: " + Suma_perimetro);
+    }
+    
+}
